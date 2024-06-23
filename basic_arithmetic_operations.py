a50f0ec071fc7b02f0c7e90f16a891711c23295b
KotaroHoriyama <kotaro.horiyama@gmail.com>
2024-06-24 02:38:24 +0900

    add addition, subtraction, multiplication and division

---
+++ basic_arithmetic_operations.py
@@ -0,0 +1,11 @@
+def main():
+    a = float(input("a = "))
+    b = float(input("b = "))
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    
+if __name__ == "__main__":
+    main()
+    
