22897ed290d8953864474f110bca700a342364f1
KotaroHoriyama <kotaro.horiyama@gmail.com>
2024-06-24 22:52:57 +0900

    GCD,LCM の追加

--- basic_arithmetic_operations.py
+++ basic_arithmetic_operations.py
@@ -1,14 +1,26 @@
-def main():
-    a = float(input("a = "))
-    b = float(input("b = "))
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+    
+def lcm(a,b):
+    return abs(a * b) / gcd(a, b)
+     
+def basic_arithmetic(a, b):
     print("a + b = ",a + b)
     print("a - b = ",a - b)
-    print("a*b = ",a*b)
+    print("a * b = ",a * b)
     if b == 0:
         print("a/b error(0 division)")
     else:
-        print("a/b = ",a/b)
-
+        print("a / b = ",a / b)
+   
+def main():
+    a = float(input("a = "))
+    b = float(input("b = "))
+    basic_arithmetic(a,b)
+    print("gcd(a, b) = ",gcd(a, b))
+    print("lcm(a, b) = ",lcm(a, b))
     
 if __name__ == "__main__":
     main()
