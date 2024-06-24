0de025acbf88e02b20cbcb5d1c10dc167b814f05
KotaroHoriyama <kotaro.horiyama@gmail.com>
2024-06-24 23:39:02 +0900

    累乗の追加

--- basic_arithmetic_operations.py
+++ basic_arithmetic_operations.py
@@ -3,11 +3,8 @@ def gcd(a, b):
         return a
     return gcd(b, a % b)
     
-def lcm(a, b):
+def lcm(a,b):
     return abs(a * b) / gcd(a, b)
-
-def pow(a, b):
-    return a**b
      
 def basic_arithmetic(a, b):
     print("a + b = ",a + b)
@@ -21,10 +18,9 @@ def basic_arithmetic(a, b):
 def main():
     a = float(input("a = "))
     b = float(input("b = "))
-    basic_arithmetic(a, b)
+    basic_arithmetic(a,b)
     print("gcd(a, b) = ",gcd(a, b))
     print("lcm(a, b) = ",lcm(a, b))
-    print("aのb乗",pow(a, b))
     
 if __name__ == "__main__":
     main()
