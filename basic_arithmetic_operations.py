4f51cd0106ed009ff6ffe9555bb430dd71fcd397
KotaroHoriyama <kotaro.horiyama@gmail.com>
2024-06-24 21:20:49 +0900

    ０割りエラー追加

--- basic_arithmetic_operations.py
+++ basic_arithmetic_operations.py
@@ -1,10 +1,14 @@
 def main():
     a = float(input("a = "))
     b = float(input("b = "))
-    print(a+b)
-    print(a-b)
-    print(a*b)
-    print(a/b)
+    print("a + b = ",a + b)
+    print("a - b = ",a - b)
+    print("a*b = ",a*b)
+    if b == 0:
+        print("a/b error(0 division)")
+    else:
+        print("a/b = ",a/b)
+
     
 if __name__ == "__main__":
     main()
